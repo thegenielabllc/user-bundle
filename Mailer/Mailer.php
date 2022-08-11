6dba9116f0b524c09c1a64103fd5e6080193bbc0
thegenielabllc <76487788+thegenielabllc@users.noreply.github.com>
2022-08-11 23:22:20 +0100

    Update Mailer.php

--- Mailer/Mailer.php
+++ Mailer/Mailer.php
@@ -108,7 +108,7 @@ class Mailer implements MailerInterface
         $subject = $subject;
         $headers = "MIME-Version: 1.0" . "\r\n";
         $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-        $headers .= "From: ".$fromEmail;
+        $headers .= "From: ".$fromEmail[0];
         $mail = mail($to,$subject,$body,$headers);
 
         if($mail){
