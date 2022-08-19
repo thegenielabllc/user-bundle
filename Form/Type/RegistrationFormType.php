8b6175995aa621fa1af51eeb2b3500253f4f2495
Muhammad Umair <mumairshamsi@gmail.com>
2022-08-19 23:39:17 +0100

    latest code

--- Form/Type/RegistrationFormType.php
+++ Form/Type/RegistrationFormType.php
@@ -39,6 +39,7 @@ class RegistrationFormType extends AbstractType
         $builder
             ->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'), array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'))
             ->add('username', null, array('label' => 'form.username', 'translation_domain' => 'FOSUserBundle'))
+            ->add('name', null, array('label' => 'Name', 'translation_domain' => 'FOSUserBundle'))
             ->add('plainPassword', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\RepeatedType'), array(
                 'type' => LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'),
                 'options' => array('translation_domain' => 'FOSUserBundle'),
